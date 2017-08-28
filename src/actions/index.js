2b5daa1f9b852c91ecb307f28c8cf70013046146
codekendo <brandonwong@fastmail.com>
2017-08-28 14:22:11 -0700

    feat: app now pulls and renders data from the redux store and redux is propgated by data from the server

---
+++ src/actions/index.js
@@ -0,0 +1,60 @@
+export const RECEIVE_CATEGORIES = "RECEIVE_CATEGORIES"
+export const RECEIVE_POSTS = "RECEIVE_POSTS"
+
+const fetchCategories = dispatch => {
+  const myInit = {
+    headers: new Headers({ Authorization: "YmxhaDpibGFo" })
+  }
+  const myRequest = new Request("http://localhost:5001/categories", myInit)
+  return fetch(myRequest, myInit).then(res => res.json()).then(data => data)
+}
+
+export const receivedCategories = data => ({
+  type: RECEIVE_CATEGORIES,
+  categories: data.categories
+})
+
+export const fetchCategoriesNow = () => dispatch => {
+  return fetchCategories().then(data => dispatch(receivedCategories(data)))
+}
+
+const fetchPosts = dispatch => {
+  const myInit = {
+    headers: new Headers({ Authorization: "YmxhaDpibGFo" })
+  }
+  const myRequest = new Request("http://localhost:5001/posts", myInit)
+  return fetch(myRequest, myInit).then(res=>res.json()).then(data=>data)
+}
+export const fetchPostsNow = () => dispatch => {
+  return fetchPosts().then(data => dispatch(receivedPosts(data)))
+}
+
+const receivedPosts =data => ({
+  type:RECEIVE_POSTS,
+  posts:data
+})
+
+/**IGNORE BELOW **/
+//tried to refactor but it did not work out since I probably need a switch statement make it work to when I send that data to the reducer
+//
+// export const receivedData = (data, thing) => {
+//   const obj = {}
+//   const capThing = thing.toUpperCase()
+//   obj["type"] = "RECEIVE_" + capThing
+//   obj[thing] = data
+//   return obj
+// }
+// const fetchURL = (thing, dispatch) => {
+//   const fetchUrl = "http://localhost:5001/" + thing
+//   const myInit = {
+//     headers: new Headers({ Authorization: "YmxhaDpibGFo" })
+//   }
+//   const myRequest = new Request(fetchUrl, myInit)
+//
+//   return fetch(myRequest, myInit).then(res => res.json())
+// }
+//
+// export const fetchFunction = thing => (dispatch) => {
+//    return fetchURL(thing, dispatch).then(data => dispatch(receivedData(data[thing], thing)))
+//
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import { render } from "react-dom"
+import "./index.css"
+import App from "./App"
+import registerServiceWorker from "./registerServiceWorker"
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import reducer from "./reducers/"
+import { Provider } from "react-redux"
+import { createLogger } from "redux-logger"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const middleware = [thunk, createLogger()];
+
+
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(...middleware))
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+
+registerServiceWorker()
